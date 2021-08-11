f1321dd74f3160d9c02400ac36c3d5f2e078712b
Simon Granger <nothem6@gmail.com>
2021-08-11 15:05:17 +0200

    yelp

---
+++ app/models/review.rb
@@ -0,0 +1,6 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, :inclusion => 0..5
+  validates :content,:rating, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+kebab = Restaurant.create!(name:'kebab', address:'rue du kebab', category:'french', phone_number:'0102030405')
+sushi = Restaurant.create!(name:'sushi', address:'rue du sushi', category:'japanese', phone_number:'0102030405')
+tacos = Restaurant.create!(name:'tacos', address:'rue du tacos', category:'french', phone_number:'0102030405')
+pizza = Restaurant.create!(name:'pizza', address:'rue du pizza', category:'italian', phone_number:'0102030405')
+mcdo = Restaurant.create!(name:'mcdo', address:'rue du mcdo', category:'french', phone_number:'0102030405')
